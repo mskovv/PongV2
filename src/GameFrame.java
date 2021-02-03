ea59ef33ecb408a0e760dced16a2369546f41034
mskovv <maslay99@gmail.com>
2021-02-03 10:46:22 +0500

    1

---
+++ src/GameFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+    GamePanel panel;
+    public GameFrame() {
+         panel = new GamePanel();
+         this.add(panel);
+         this.setTitle("Pong");
+         this.setResizable(false);
+         this.setBackground(Color.BLACK);
+         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         this.pack();
+         this.setVisible(true);
+         this.setLocationRelativeTo(null);
+    }
+}
